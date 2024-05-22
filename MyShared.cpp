00ebe163baaf77b5c68f9057411f021b48ffc9ec
vadimkriv <polenko1029@gmail.com>
2024-05-22 16:04:18 +0300

    Update MyShared.cpp

--- MyShared.cpp
+++ MyShared.cpp
@@ -1,67 +1,46 @@
+
 #include <iostream>
-template<class T>
+
+template <typename T>
 class MyShared {
-    T * p;
-    int * ref_count;
 public:
-    explicit MyShared(T *p = nullptr) : p(p), ref_count(new int(1)) {}
-    ~MyShared() {
-        release();
+  std::shared_ptr<T> p;
+
+  MyShared(T* ptr) : p(std::make_shared<T>(*ptr)) {}
+  ~MyShared() = default;
+
+  T* get() const { return p.get(); }
+  T& operator*() { return *p; }
+  T* operator->() { return p.get(); }
+
+  MyShared(MyShared&& other) noexcept {
+    p = std::move(other.p);
+  }
+  MyShared& operator=(MyShared&& other) noexcept {
+    if (this != &other) {
+      p = std::move(other.p);
     }
-    MyShared(const MyShared& other) : p(other.p), ref_count(other.ref_count) {
-        ++(*ref_count);
-    }
-    MyShared& operator=(const MyShared& other) {
-        if (this != &other) {
-            release();
-            p = other.p;
-            ref_count = other.ref_count;
-            ++(*ref_count);
-        }
-        return *this;
-    }
-    MyShared(MyShared&& other) noexcept : p(other.p), ref_count(other.ref_count) {
-        other.p = nullptr;
-        other.ref_count = nullptr;
-    }
-    MyShared& operator=(MyShared&& other) noexcept {
-        if (this != &other) {
-            release();
-            p = other.p;
-            ref_count = other.ref_count;
-            other.p = nullptr;
-            other.ref_count = nullptr;
-        }
-        return *this;
-    }
-    T * get() const {
-        return p;
-    }
-    T & operator*() {
-        return *p;
-    }
-    T * operator->() {
-        return p;
-    }
-private:
-    void release() {
-        if (ref_count) {
-            --(*ref_count);
-            if (*ref_count == 0) {
-                delete p;
-                delete ref_count;
-            }
-        }
-    }
+    return *this;
+  }
 };
-template<typename T, typename... Args>
-MyShared<T> Make_MyShared(Args&&... args) {
-    return MyShared<T>(new T(std::forward<Args>(args)...));
+
+template <typename T>
+MyShared<T> Make_MyShared(T* ptr) {
+  return MyShared<T>(ptr);
 }
+
 int main() {
-    auto p1 = Make_MyShared<MyPoint>(5, 10);
-    std::cout << "Point: " << p1->x << ", " << p1->y << std::endl;
-    MyShared<MyPoint> p2 = p1;
-    std::cout << "Point after copy: " << p2->x << ", " << p2->y << std::endl;
-    return 0;
+  class MyPoint {
+  public:
+    int x, y;
+
+    MyPoint(int x_, int y_) : x(x_), y(y_) {}
+  };
+  MyShared<MyPoint> ptr = Make_MyShared(new MyPoint(5, 10));
+  std::cout << ptr->x << " " << ptr->y << std::endl;
+  (*ptr).x = 15;
+  std::cout << ptr->x << " " << ptr->y << std::endl;
+  MyShared<MyPoint> ptr2 = std::move(ptr);
+  std::cout << ptr2->x << " " << ptr2->y << std::endl;
+  return 0;
 }
